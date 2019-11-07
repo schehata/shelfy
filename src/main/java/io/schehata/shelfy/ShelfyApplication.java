319eb2c3dde7d47f53bb6246906afba125971f90
Islam Shehata <islam.shehata@friday.de>
2019-11-07 14:19:57 +0100

    init

---
+++ src/main/java/io/schehata/shelfy/ShelfyApplication.java
@@ -0,0 +1,13 @@
+package io.schehata.shelfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/schehata/shelfy/ShelfyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.schehata.shelfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
